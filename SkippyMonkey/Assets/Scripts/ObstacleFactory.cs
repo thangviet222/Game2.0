2e3b5537b95d3d499c03fc8aed5782e67e8c0d7c
thangviet222 <thangnvse05071@fpt.edu.vn>
2017-06-12 08:19:29 +0700

    BTVN 12/6

---
+++ SkippyMonkey/Assets/Scripts/ObstacleFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleFactory : MonoBehaviour {
+    public GameObject obstaclePrefab;
+	// Use this for initialization
+	void Start () {
+        int y = -250;
+        while (y < 568)
+        {
+            int x = Random.Range(-250, 250);
+            GameObject newObstacle = Instantiate(obstaclePrefab,
+                new Vector3(x, y, 10),
+                Quaternion.identity);
+            y = y + 200;
+        }
+    }
+	
+	
+}
---
+++ SkippyMonkey/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Touch;
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+public class PlayerControler : MonoBehaviour {
+
+    private static float SCREEN_HALF_WIDTH = 320f;
+    public float playerSpeed;
+    public float PlayerJumpSpeed;
+    private Rigidbody2D rgbody;
+    private Animator anim;
+
+    private void Start()
+    {
+        rgbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        rgbody.velocity = rgbody.velocity.WithX(playerSpeed);
+        LeanTouch.OnFingerTap += Jump;
+    }
+    // Update is called once per frame
+
+    private void Update () {
+        rgbody.velocity = rgbody.velocity.WithX(playerSpeed);
+        if (transform.position.x > SCREEN_HALF_WIDTH)
+        {
+            transform.position = transform.position.WithX(
+                transform.position.x - 2*SCREEN_HALF_WIDTH);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rgbody.velocity = rgbody.velocity.WithY(PlayerJumpSpeed);
+            anim.SetBool("isGrounded", false);
+        }
+
+    }
+    private void OnDestroy()
+    {
+        LeanTouch.OnFingerTap -= Jump;
+    }
+    private void Jump(LeanFinger finger)
+    {
+
+        rgbody.velocity = rgbody.velocity.WithY(PlayerJumpSpeed);
+        anim.SetBool("isGrounded", false);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        anim.SetBool("isGrounded", true);
+    }
+}
